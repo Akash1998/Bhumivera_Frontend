7b2094b01def398ebc112804b7ba6f68a146e0ed
anritvox <anritvox@gmail.com>
2026-05-10 10:42:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { SettingsProvider } from "./context/SettingsContext.jsx";
-
-// 100% Clean Entry Point - Purged Conflicting ReCaptcha Provider
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
-  </StrictMode>
-);
+import React,{StrictMode}from"react";
+import{createRoot}from"react-dom/client";
+import"./index.css";
+import App from"./App.jsx";
+import{SettingsProvider}from"./context/SettingsContext.jsx";
+import{ToastProvider}from"./context/ToastContext.jsx";
+createRoot(document.getElementById("root")).render(<StrictMode><SettingsProvider><ToastProvider><App/></ToastProvider></SettingsProvider></StrictMode>);
