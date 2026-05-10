ab129f9f77ea4af2043f127c103f012cdd2ed8f3
anritvox <anritvox@gmail.com>
2026-05-10 10:43:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React,{StrictMode}from"react";
-import{createRoot}from"react-dom/client";
-import"./index.css";
-import App from"./App.jsx";
-import{SettingsProvider}from"./context/SettingsContext.jsx";
-import{ToastProvider}from"./context/ToastContext.jsx";
-createRoot(document.getElementById("root")).render(<StrictMode><SettingsProvider><ToastProvider><App/></ToastProvider></SettingsProvider></StrictMode>);
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { SettingsProvider } from "./context/SettingsContext.jsx";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
+  </StrictMode>
+);
